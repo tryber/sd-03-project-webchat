f83eb2a1405ccecbcb3f103e8991e340ac2c0e6e
fabemiliano <fab.emiliano@gmail.com>
2020-10-28 20:35:36 -0300

    first try

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const bodyParser = require('body-parser');
+const { saveMessageInDB, getPreviousMessages } = require('./models/model');
+const { formatDate } = require('./services/services');
+
+const PUBLIC_PATH = path.join(__dirname, 'public');
+app.use(bodyParser.json());
+app.use('/', express.static(PUBLIC_PATH, { extensions: ['html', 'css'] }));
+
+let usersArr = [];
+
+io.on('connection', async (socket) => {
+  const previousMessages = await getPreviousMessages();
+  const formatedPreviousMessages = previousMessages
+    .map(({ chatMessage, nickname, date }) => ({ chatMessage, nickname, date: formatDate(date) }));
+
+  socket.emit('previousMessages', formatedPreviousMessages);
+
+  socket.on('newUser', ({ nickname }) => {
+    console.log('nickname', nickname);
+    usersArr.push({ socketId: socket.id, nickname });
+    const users = usersArr.map(({ nickname: name }) => name);
+    io.emit('listOfUsers', users);
+  });
+
+  socket.on('nameChanged', ({ nickname }) => {
+    usersArr.push({ socketId: socket.id, nickname });
+    const users = usersArr.map(({ nickname: name }) => name);
+    io.emit('listOfUsers', users);
+  });
+
+  socket.on('disconnect', () => {
+    usersArr = usersArr.filter(({ socketId }) => socketId !== socket.id);
+    const users = usersArr.map(({ nickname: name }) => name);
+    io.emit('listOfUsers', users);
+  });
+
+  socket.on('message', async ({ chatMessage, nickname }) => {
+    const { chatMessage: message, nickname: nick, date } = await
+    saveMessageInDB({ chatMessage, nickname });
+    const newDate = formatDate(date);
+    io.emit('message', `${newDate} : ${nick} - ${message}`);
+  });
+});
+
+http.listen(3000, () => console.log('ouvindo a porta 3000'));
