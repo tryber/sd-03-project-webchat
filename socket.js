ba6935547edabdfe065431e38427e4efa63d8c5d
gabryelryba <gabryel.ryba@gmail.com>
2020-10-29 17:52:59 -0300

    render message history feature

--- socket.js
+++ socket.js
@@ -1,6 +1,7 @@
 const http = require('http');
-
 const socketIo = require('socket.io');
+
+const messages = require('./models/messagesModel');
 
 const messengerController = require('./controllers/messengerController');
 
@@ -8,7 +9,11 @@ module.exports = () => {
   const httpServer = http.createServer();
   const io = socketIo(httpServer);
 
-  io.on('connection', (socket) => {
+  io.on('connection', async (socket) => {
+    const history = await messages.getAll();
+
+    socket.emit('messageHistory', history);
+
     socket.on('message', (data) => {
       messengerController.sendMessage(socket, data);
       socket.broadcast.emit('messageReceived', data);
