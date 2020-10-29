92ea44e48570befb5accd6fe0e4c323c667cb686
Marco Barbosa <marco.meireles.b@gmail.com>
2020-10-29 15:38:03 -0300

    Passando nos requisitos 2 e 4

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,15 +1,3 @@
-// const sendNotification = (io) => (req, res) => {
-//   const { text } = req.body;
-
-//   if (!text) {
-//     return res.status(422).json({ message: 'Missing notification text' });
-//   }
-
-//   io.emit('notification', text);
-
-//   res.status(200).json({ message: 'Notification sent' });
-// };
-
 const handleNotificationEvent = (io, notifications) => async (text) => {
   const { chatMessage, nickname } = text;
   const timestamp = new Date();
@@ -17,13 +5,14 @@ const handleNotificationEvent = (io, notifications) => async (text) => {
   io.emit('message', text);
 };
 
-const handleNameChangeEvent = (socket) => async (text) => {
-  socket.broadcast.emit('nameChange', text);
-  socket.emit('nameChange', text);
+const handleNameChangeEvent = (socket, usersList) => async (text) => {
+  usersList.splice(usersList.indexOf(socket), 1);
+  usersList.push(text.newNickname);
+  socket.broadcast.emit('nameChange', { newNickname: usersList });
+  socket.emit('nameChange', { newNickname: usersList });
 };
 
 module.exports = {
-  // sendNotification,
   handleNotificationEvent,
   handleNameChangeEvent,
 };
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const path = require('path');
-// const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
 const http = require('http').createServer(app);
 const socketIO = require('socket.io');
+
 const io = socketIO(http);
 
 const notificationController = require('./controllers/notificationController');
@@ -12,33 +12,32 @@ const notifications = require('./services/messageService');
 
 const PUBLIC_PATH = path.join(__dirname, 'public');
 
-// app.use(bodyParser.json());
-// app.use('/', express.static(PUBLIC_PATH, { extensions: ['html'] }));
 app.get('/', (req, res) => {
-  res.sendFile(PUBLIC_PATH + '/index.html');
+  res.sendFile(`${PUBLIC_PATH}/index.html`);
 });
 
 const usersList = [];
 let guestId = 0;
-
-// socket.emit('nameChange', { newNickname: socket.guest });
+let usersPool = '';
 
 io.on('connection', async (socket) => {
-  guestId++;
-  socket.guest = `Anônimo-${guestId}`;
-  usersList.push(socket.guest);
-  console.log(usersList);
-  usersList.map((user) => socket.emit('nameChange', { newNickname: user }));
+  guestId += 1;
+  usersPool = `Anônimo-${guestId}`;
+  usersList.push(usersPool);
+  socket.emit('nameChange', { newNickname: usersList });
+  socket.broadcast.emit('nameChange', { newNickname: usersList });
 
-  // socket.broadcast.emit('message', { chatMessage: 'Usuário Anônimo Conectado', nickname: 'Bate-Papo' });
   const allNotifications = await notifications.getAllMessagesService();
   socket.emit('history', allNotifications);
-  socket.on('message', notificationController.handleNotificationEvent(io, notifications));
-  socket.on('nameChange', notificationController.handleNameChangeEvent(socket));
-});
 
-io.on('disconnecting', async (socket) => {
-  socket.emit('userDisconnect', socket);
+  socket.on('message', notificationController.handleNotificationEvent(io, notifications));
+
+  socket.on('nameChange', notificationController.handleNameChangeEvent(socket, usersList));
+
+  socket.on('disconnect', () => {
+    usersList.splice(usersList.indexOf(socket), 1);
+    io.emit('nameChange', { newNickname: usersList });
+  });
 });
 
 http.listen(3000, () => {
