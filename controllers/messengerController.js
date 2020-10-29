6c9df1f5398c2c310920b938457e2315ff9540b6
gabryelryba <gabryel.ryba@gmail.com>
2020-10-29 17:24:30 -0300

    message emitted at the front end + date format

--- controllers/messengerController.js
+++ controllers/messengerController.js
@@ -1,7 +1,6 @@
 const messages = require('../models/messagesModel');
 
 const sendMessage = async (socket, messageObject) => {
-  socket.emit('messageReceived', messageObject);
   const res = await messages.store(messageObject);
   return res;
 };
