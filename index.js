ace5dadd74d3ef1d1e8d6dba2d63f4cca69cd1df
William Igor <williamigorsilva@gmail.com>
2020-10-29 17:25:09 -0300

    setup inicial

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 const path = require('path');
-const app = require('express')();
+const express = require('express');
+
+const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-
 const bodyParser = require('body-parser');
+const connect = require('./models/connection');
 
 app.use(bodyParser.json());
+app.use('/client', express.static(path.join(__dirname, '/index.html'), { extensions: ['html'] }));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
-});
+app.get('/', (_req, res) => {
+  io.on('connection', (socket) => {
+    console.log('aqui');
+    socket.on('message', ({ chatMessage, nickname }) => {
+      console.log('teste', chatMessage, nickname);
+      return connect()
+        .then((db) => db.collection('webchat').insertOne({ chatMessage, nickname }));
+    });
 
-app.post('/post', (req, res) => {
-  const { name } = req.body;
-
-});
- 
-io.on('connection', (socket) => {
-  console.log('Conectado');
-  socket.on('disconnect', () => {
-    console.log('Desconectado');
+    socket.on('message', (data) => {
+      socket.broadcast.emit('message', data);
+    });
   });
+  return res.status(200);
 });
 
 http.listen(3000, () => {
--- models/connection.js
+++ models/connection.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require('mongodb')
+const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
 let schema = null;
