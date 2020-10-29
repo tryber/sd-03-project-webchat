4187c31a64687f234719321f09fcf126debf30a7
Cardoso <lsc.cardosoo@gmail.com>
2020-10-29 11:00:10 -0300

    Testes

--- http.js
+++ http.js
@@ -1,9 +1,6 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-// const { idValidate } = require('./middlewares/validate');
-
-// const userController = require('./controllers/userController');
 
 const app = express();
 
@@ -13,8 +10,5 @@ module.exports = () => {
   app.use(bodyParser.json());
 
   app.use('/', express.static(PUBLIC_PATH, { extensions: ['html'] }));
-  // app.post('/message', messageController.sendMessage(io));
-  // app.post('/', userController.registerUser(io));
-
   return app;
 };
