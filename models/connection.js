1b621429bc6db64d2802ddfcd7672c980b48c0e8
“brunobatista” <brunobatista101@gmail.com>
2020-10-29 13:49:56 -0300

    testeavaliador

---
+++ models/connection.js
@@ -0,0 +1,13 @@
+const mongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+
+module.exports = () => mongoClient
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((conn) => conn.db(process.env.DB_NAME))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
---
+++ services/services.js
@@ -0,0 +1,16 @@
+const formatDate = (date) => {
+  let day = date.getDay();
+  day = day < 10 ? `0${day}` : day;
+  let month = date.getMonth() + 1;
+  month = month < 10 ? `0${month}` : month;
+  const year = date.getFullYear();
+  let hours = date.getHours();
+  hours = hours < 10 ? `0${hours}` : hours;
+  let minutes = date.getMinutes();
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+  let seconds = date.getSeconds();
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
+  return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+};
+
+module.exports = { formatDate };
