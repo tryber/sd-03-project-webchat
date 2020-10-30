7c727df90354227b1cc206c17523b95226e4496a
Clayton Pereira <cpereiramt@gmail.com>
2020-10-30 11:15:44 -0400

    Salvando alteraçoes no projeto

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
-const net = require('net');
+const express = require('express');
+const path = require('path');
 
-const port = 3000;
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const bodyParser = require('body-parser');
+
+const PATH_STATIC = path.join(`${__dirname}/public`);
+app.use(bodyParser.json());
+app.use('/', express.static('/public', { extensions: ['html'] }));
+
+const PORT = 3000;
 const sockets = [];
-let guestId = 0;
+const guestId = 0;
 const broadcastMessage = (from, message) => {
   sockets
     .filter((socket) => socket.guest !== from)
     .forEach((socket) => socket.write(message));
 };
 
-const onNewconnection = (socket) => {
-  guestId++;
-  socket.guest = `Guest ${guestId}`;
+app.get('/', (req, res) => {
+  console.log(PATH_STATIC);
+  res.sendFile(`${PATH_STATIC}/client.html`);
+});
+io.on('connection', (socket) => {
+  /*  guestId += 1; */
+  /*  socket.guest = `Guest ${guestId}`;
   sockets.push(socket);
   socket.write('boas vindas ao chat !');
   broadcastMessage(socket.guest, `${socket.guest} entrou no chat`);
@@ -23,12 +37,10 @@ const onNewconnection = (socket) => {
   socket.on('data', (data) => {
     const message = `${socket.guest} > ${data.toString('utf-8')}`;
     broadcastMessage(socket.guest, message);
-  });
+  }); */
   socket.on('error', (err) => console.log('Erro no socket', err));
-};
+});
 
-const server = net.createServer(onNewconnection);
-
-server.on('error', (err) => console.log('Erro no server', err));
-
-server.listen(port, () => console.log('server listening on', port));
+/* server.on('error', (err) => console.log('Erro no server', err));
+ */
+http.listen(PORT, () => console.log('Servidor ouvindo na porta 3000'));
