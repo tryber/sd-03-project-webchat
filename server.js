1e4e98ab7ff48f513597681ca1d6c1aa3eab8bed
William Igor <williamigorsilva@gmail.com>
2020-10-29 22:18:01 -0300

    evoluindo

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const path = require('path');
 const express = require('express');
 
+const moment = require('moment');
+
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const bodyParser = require('body-parser');
 
+const PUBLIC_PATH = path.join(__dirname, 'public');
 
+app.use(bodyParser.json());
+app.use('/', express.static(PUBLIC_PATH, { extensions: ['html'] }));
 
-app.use('/client', express.static(path.join(__dirname, '/index.html'), { extensions: ['html'] }));
-
-
-http.listen(3000, () => {
-  console.log('Servidor ouvindo na porta 3000');
+io.on('connection', async (socket) => {
+  socket.on('message', ({ chatMessage, nickname }) => {
+    const timesTamp = new Date();
+    // const timesTamp = new Date().toLocaleDateString() formata local
+    const time = moment(timesTamp).format('D-M-yyyy hh:mm:ss');
+    const renderMessagens = `${nickname} Time: ${time} Say: ${chatMessage}`;
+    socket.emit('message', renderMessagens);
+    socket.broadcast.emit('message', renderMessagens);
+  });
 });
+
+http.listen(3000, () => console.log('Servidor ouvindo na porta 3000'));
