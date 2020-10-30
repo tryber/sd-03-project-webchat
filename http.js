b121f8a6fd18ab71c7634dc4c376e4ee9c43b8de
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-10-30 12:19:25 -0300

    better than whatsapp

--- http.js
+++ http.js
@@ -6,7 +6,7 @@ const app = express();
 
 const PUBLIC_PATH = path.join(__dirname);
 
-module.exports = (io) => {
+module.exports = () => {
   app.use(bodyParser.json());
 
   app.use('/', express.static(PUBLIC_PATH, { extensions: ['html'] }));
