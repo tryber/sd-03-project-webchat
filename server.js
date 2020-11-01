a3eac8cb64b179206ee1c6b663e197dc6fedced5
Clayton Pereira <cpereiramt@gmail.com>
2020-11-01 09:32:47 -0400

    primeiro requisito completo

--- server.js
+++ server.js
@@ -12,22 +12,27 @@ app.use('/', express.static('/public', { extensions: ['html'] }));
 
 const PORT = 3000;
 const sockets = [];
-let guestId = 0;
-
+/* const guestId = 0;
+ */
 app.get('/', (req, res) => {
   res.sendFile(`${PATH_STATIC}/client.html`);
 });
 io.on('connect', (socket) => {
-  guestId += 1;
-  socket.nickname = guestId;
+/*   guestId += 1;
+*/
+// socket.nickname = guestId;
   socket.on('error', (err) => console.log('Erro no socket', err));
 
   socket.on('message', (msg) => {
-    const { chatMessage } = msg;
-    socket.msg = chatMessage;
+    const dateObj = new Date();
+    const date = `${dateObj.getDate()}-${dateObj.getMonth()}-${dateObj.getFullYear()}`;
+    const time = `${dateObj.getHours()}:${dateObj.getMinutes()}:${dateObj.getSeconds()}`;
+    console.log(date);
+    const { chatMessage, nickname } = msg;
+    // socket.msg = chatMessage;
     sockets.push(socket);
-    socket.broadcast.emit('message', chatMessage);
-    console.log(`Guest ${guestId} disse >  ${socket.msg}`);
+    socket.broadcast.emit('message', `${nickname} ${chatMessage} ${date} ${time}`);
+    console.log(`Guest ${nickname} disse >  ${socket.msg}`);
   });
 });
 
