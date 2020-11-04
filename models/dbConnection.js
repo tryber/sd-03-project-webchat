98b727597add94f19505f0e61d4d96d51e2ada01
Clayton Pereira <cpereiramt@gmail.com>
2020-11-04 13:09:11 -0400

    Salvando alteraçoes requisito 3

--- models/dbConnection.js
+++ models/dbConnection.js
@@ -3,15 +3,15 @@ const { MongoClient } = require('mongodb');
 
 const { DB_URL, DB_NAME } = process.env;
 
-/* const dbURL = MONGO_DB_URL || 'mongodb://localhost:27017/webchat';
+const dbURL = DB_URL || 'mongodb://localhost:27017';
 const dbName = DB_NAME || 'webchat';
- */
+
 const connect = () =>
-  MongoClient.connect(DB_URL, {
+  MongoClient.connect(dbURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-    .then((connection) => connection.db(DB_NAME))
+    .then((connection) => connection.db(dbName))
     .catch((err) => {
       console.error(err);
       process.exit(1);
