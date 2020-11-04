88e9818ea15c735dd26243ab2436fc44b2664380
Clayton Pereira <cpereiramt@gmail.com>
2020-11-04 01:03:11 -0400

    tentando corrigir erro no github connection refused

--- models/dbConnection.js
+++ models/dbConnection.js
@@ -1,17 +1,17 @@
 require('dotenv/config');
 const { MongoClient } = require('mongodb');
 
-const { MONGO_DB_URL, DB_NAME } = process.env;
+const { DB_URL, DB_NAME2} = process.env;
 
-const dbURL = MONGO_DB_URL || 'mongodb://localhost:27017/webchat';
+/* const dbURL = MONGO_DB_URL || 'mongodb://localhost:27017/webchat';
 const dbName = DB_NAME || 'webchat';
-
+ */
 const connect = () =>
-  MongoClient.connect(dbURL, {
+  MongoClient.connect(DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-    .then((connection) => connection.db(dbName))
+    .then((connection) => connection.db(DB_NAME2))
     .catch((err) => {
       console.error(err);
       process.exit(1);
