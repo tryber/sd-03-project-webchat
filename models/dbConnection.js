1726cde7d4d8d1943b48d1247e3c0d64f4e04088
Clayton Pereira <cpereiramt@gmail.com>
2020-11-04 15:23:57 -0400

    testando  a soluçao de erro no github

--- models/dbConnection.js
+++ models/dbConnection.js
@@ -1,7 +1,7 @@
 const mongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
-const dbURL = process.env.DB_URL || 'mongodb://localhost:27017';
+const dbURL = process.env.DB_URL || 'localhost:27017';
 const dbName = process.env.DB_NAME || 'webchat';
 
 let schema = null;
