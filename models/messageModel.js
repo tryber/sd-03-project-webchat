2434389496faa539d1bdfcd923d3f72b23d6453f
nato-re <alvares.renato@hotmail.com>
2020-11-04 23:50:57 -0300

    trying to do bonus

--- models/messageModel.js
+++ models/messageModel.js
@@ -1,11 +1,33 @@
 const getGeneral = (db) => () => db.collection('general').find({}).sort({ date: 1 }).toArray();
 
-const insertGeneral = (db) => ({ chatMessage, nickname, date }) => db.collection('general').insertOne({ chatMessage, nickname, date })
+const getPrivate = (db) => (users) => db.collection('private').find({ users: { $all: users } }).sort({ date: 1 }).toArray();
+
+// TODO upsert
+
+const insertGeneral = (db) => ({ chatMessage, nickname }) => db.collection('general').insertOne({ chatMessage, nickname })
+  .then((value) => value.ops[0]);
+
+const insertPrivate = (db) => ({ chatMessage, users }) => db.collection('private')
+  .updateOne(
+    { users: { $all: users } },
+    {
+      $setOnInsert: {
+        users,
+        $push: {
+          messages: [chatMessage],
+        },
+      },
+    },
+    { upsert: true },
+  )
+
   .then((value) => value.ops[0]);
 
 const factory = (connect) => ({
   getGeneral: getGeneral(connect),
+  getPrivate: getPrivate(connect),
   insertGeneral: insertGeneral(connect),
+  insertPrivate: insertPrivate(connect),
 });
 
 module.exports = factory;
--- socket.js
+++ socket.js
@@ -1,3 +1,10 @@
+const messageWithDate = (nickname, chatMessage) => {
+  const newDate = `${new Date().getUTCDate()}-${new Date().getMonth()}-${new Date().getFullYear()}`;
+  const date = new Date().toLocaleTimeString('pt-BR');
+
+  return { string: `(${date} ${newDate}) ${nickname}: ${chatMessage}`, date };
+};
+
 const emitHistory = async (messageModel, socket) => {
   const messages = await messageModel.getGeneral();
 
@@ -12,11 +19,36 @@ module.exports = (io, messageModel) => {
   io.on('connection', async (socket) => {
     await emitHistory(messageModel, socket);
 
+    socket.on('privateHistory', async (nickname) => {
+      const user1 = users.find((u) => u.id === socket.id);
+      const index1 = users.indexOf(user1);
+      const user2 = users.find((u) => u.nickname === nickname);
+      const index2 = users.indexOf(user2);
+
+      const usersArray = [users[index1], users[index2]];
+
+      const messages = await messageModel.getPrivate(usersArray);
+      socket.emit('sendPrivateHistory', messages);
+    });
+
+    socket.on('private', async ({ nickname, message }) => {
+      const user1 = users.find((u) => u.id === socket.id);
+      const index1 = users.indexOf(user1);
+      const user2 = users.find((u) => u.nickname === nickname);
+      const index2 = users.indexOf(user2);
+      console.log('pv', nickname, message)
+      console.log('users', users)
+      const usersArray = [users[index1].id, users[index2].id];
+      const chatMessage = messageWithDate(nickname, message);
+      await messageModel.insertPrivate({ chatMessage, users: usersArray });
+      io.clients[user2.socket.id].emit('private', chatMessage);
+    });
+
     socket.on('userConn', (nickname) => {
-      console.log('users before', users);
+      // console.log('users before', users);
 
       users.push({ nickname, id: socket.id });
-      console.log('users on conn:', users);
+      // console.log('users on conn:', users);
       io.emit('usersOnline', users);
     });
 
@@ -30,24 +62,22 @@ module.exports = (io, messageModel) => {
     socket.on('disconnecting', () => {
       // on user disconnect pass socket.id
       // console.log(socket.id);
-      console.log('users on disconnecting:', users);
+      // console.log('users on disconnecting:', users);
 
       const user = users.find((u) => u.id === socket.id);
-      console.log('disconnecting user:', user);
+      // console.log('disconnecting user:', user);
       // io.emit('menssage', `${socket.id} saiu`);
 
       const index = users.indexOf(user);
       users.splice(index, 1);
-      console.log('users on disc', users);
+      // console.log('users on disc', users);
       io.emit('usersOnline', users);
     });
 
     socket.on('message', async ({ chatMessage, nickname }) => {
-      const newDate = `${new Date().getUTCDate()}-${new Date().getMonth()}-${new Date().getFullYear()}`;
-      const date = new Date().toLocaleTimeString('pt-BR');
-
+      const { string, date } = messageWithDate(nickname, chatMessage);
       await messageModel.insertGeneral({ chatMessage, nickname, date });
-      io.emit('message', `(${date} ${newDate}) ${nickname}: ${chatMessage}`);
+      io.emit('message', string);
     });
   });
 
