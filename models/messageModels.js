1214c9b0caf271dee09849337cf9ff23dcf0a03d
Gustavo <ggteodor07@gmai.com>
2020-11-08 20:48:02 -0300

    third req

---
+++ models/messageModels.js
@@ -0,0 +1,22 @@
+const connection = require('../tests/helpers/db');
+
+const getAllmessages = async () => (
+  connection().then((db) => (db).collection('messages').find({}).toArray())
+);
+
+const saveMessage = async (time, date, chatMessage, nickname) => (
+  connection()
+    .then((db) => (
+      db.collection('messages').insertOne({ time, date, chatMessage, nickname })))
+    .then(() => ({
+        time,
+        date,
+        chatMessage,
+        nickname,
+    }))
+);
+
+module.exports = {
+  getAllmessages,
+  saveMessage,
+};
--- server.js
+++ server.js
@@ -4,21 +4,24 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path')
+const { getAllmessages, saveMessage } = require('./models/messageModels')
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 io.on('connection', async (socket) => {
-  console.log(
-    'aqui'
-  );
+  const savedMessages = await getAllmessages();
+  socket.emit('history', savedMessages);
+
   socket.on('message', async ({ chatMessage, nickname }) => {
     const date = new Date();
     const msgDate = date.toLocaleDateString('pt-BR').replace(/\//g, '-');
     const msgTime = date.toLocaleTimeString('pt-BR');
+    await saveMessage(msgTime, msgDate, chatMessage, nickname);
     io.emit('message', `${msgDate} ${msgTime} - ${nickname}: ${chatMessage}`);
   });
+
 });
 
 http.listen(3000, () => {
