4ea544182b10932f071d9654fc796902eabab07d
nato-re <alvares.renato@hotmail.com>
2020-11-08 12:39:34 -0300

    testing Batista's connection of mongo

--- models/connection.js
+++ models/connection.js
@@ -1,22 +1,23 @@
-const { MongoClient } = require('mongodb');
-require('dotenv/config');
+const mongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
 
-/* const {
-  DB_URL = 'mongodb://localhost:27017',
-  DB_NAME = 'webchat',
-} = process.env; */
+let schema = null;
 
-let schema;
-
-module.exports = () => {
+const connection = () => {
   if (schema) return Promise.resolve(schema);
-  return MongoClient.connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-    .then((connection) => connection.db(process.env.DB_NAME))
+  return mongoClient
+    .connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((conn) => conn.db(process.env.DB_NAME))
+    .then((dbSchema) => {
+      schema = dbSchema;
+      return schema;
+    })
     .catch((err) => {
       console.error(err);
       process.exit(1);
     });
 };
+module.exports = connection;
