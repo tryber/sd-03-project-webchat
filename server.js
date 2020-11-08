51a047e26f52eaff78839246fa687d2ba28e10c5
Gustavo <ggteodor07@gmai.com>
2020-11-08 18:06:36 -0300

    first and second requisit

--- server.js
+++ server.js
@@ -6,19 +6,18 @@ const io = require('socket.io')(http);
 const path = require('path')
 
 app.get('/', (req, res) => {
- 
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 io.on('connection', async (socket) => {
   console.log(
-    'Usuário conectado, igual ao que fizemos na aula anterior, porém dessa vez em um servidor escalável'
+    'aqui'
   );
-  socket.on('message', ({ chatMessage, nickname }) => {
+  socket.on('message', async ({ chatMessage, nickname }) => {
     const date = new Date();
     const msgDate = date.toLocaleDateString('pt-BR').replace(/\//g, '-');
     const msgTime = date.toLocaleTimeString('pt-BR');
-    io.emit(`${msgDate} ${msgTime} - ${nickname}: ${chatMessage}`);
+    io.emit('message', `${msgDate} ${msgTime} - ${nickname}: ${chatMessage}`);
   });
 });
 
