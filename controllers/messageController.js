86fbd348023525f54ce1cdd137e0ef817b5f6948
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-11-08 15:29:28 -0300

    requisito 2 passando

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,15 +1,23 @@
 const rescue = require('express-rescue');
 
+// Referência usada para criar a data no formato esperado:
+// https://github.com/tryber/sd-03-project-webchat/pull/13/files#diff-112ac233b1e3314b31d86e70dd54fd358cf3e246c38a63bf2e3085a47a0d6cf3
+
 const sendMessage = (io) =>
   rescue((req) => {
     try {
       const { chatMessage, nickname } = req;
 
+      const date = `${new Date().getUTCDate()}-${new Date().getMonth()}-${new Date().getFullYear()}`;
+      const time = new Date().toLocaleTimeString('pt-BR');
+
       if (!chatMessage) {
         throw new Error('Missing message');
       }
 
-      io.emit('message', { chatMessage, nickname });
+      const message = `${date} ${time} - ${nickname}: ${chatMessage}`;
+
+      io.emit('message', message);
     } catch (err) {
       console.error(err);
     }
