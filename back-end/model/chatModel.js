7e91959fd5ebea8ccda42f4e12e0618f04e096c9
luis.campos <gucampos845@gmail.com>
2020-11-08 13:31:24 -0300

    Correction in the model

--- back-end/model/chatModel.js
+++ back-end/model/chatModel.js
@@ -1,11 +1,9 @@
 const connection = require('./connect');
 
-const createChat = async (nick, message, date) =>
+const createChat = async (nickname, message, date) =>
   connection().then((db) =>
     db.collection('message')
-      .insertOne({ 'nickname', 'message', 'date' })
-      .then(() => ({ nick, message, date }))
-  );
+      .insertOne({ nickname, message, date }));
 
 const readChat = async () => connection().then((db) => db.collection('message').find({}).toArray());
 
