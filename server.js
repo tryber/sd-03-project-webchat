7597c86141f91c305b52740a19672a3fda295cda
DiegoRafaelAF <draf0314@gmail.com>
2020-11-08 23:06:33 -0300

    Alteração do código para cumprimento do requisito 4

--- server.js
+++ server.js
@@ -1,41 +1,65 @@
-const express = require('express');
-const path = require('path');
 const moment = require('moment');
+const path = require('path');
+const express = require('express');
 
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const http = require('http').createServer(app);
+const socketIO = require('socket.io');
 
-const { getAllMessage, saveMessage } = require('./models/messageModel');
+const io = socketIO(http);
+const { saveMessage, getAllMessage } = require('./models/messageModel');
 
-const PORT = 3000;
+const PUBLIC_PATH = path.join(__dirname, 'public');
+app.use('/', express.static(PUBLIC_PATH, { extensions: ['html'] }));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', (_req, res) => {
-  res.render('index.html');
-});
+const userInObj = {};
 
 io.on('connection', async (socket) => {
-  const previousMessage = await getAllMessage();
+  // gero um nome aleatório e salvo ele em um obj { idDoSocket : nomeAleatório}
+  const getRandomName = () => `User ${Math.ceil(Math.random() * 100)}`;
 
-  previousMessage.forEach(({ chatMessage, timestamp, nickname }) => {
-    const message = `${nickname} ${timestamp} ${chatMessage}`;
-    socket.emit('history', message);
+  userInObj[socket.id] = getRandomName();
+  const newNickname = Object.values(userInObj);
+
+  socket.emit('changeName', { newNickname });
+  socket.broadcast.emit('changeName', { newNickname });
+
+  // chat persistence
+  const allMessages = await getAllMessage();
+  const messageArray = [];
+  allMessages.map((e) => {
+    const { nickname, chatMessage, timestamp } = e;
+    const time = moment(timestamp).format('D-M-yyyy hh:mm:ss');
+    const messageToSend = `chatMessage: ${time} ${nickname} Say: ${chatMessage}`;
+    return messageArray.push(messageToSend);
+  });
+  socket.emit('history', messageArray);
+  socket.broadcast.emit('history', messageArray);
+  // chat message
+  socket.on('message', async (message) => {
+    const { chatMessage, nickname } = message;
+    const timestamp = new Date();
+    await saveMessage(chatMessage, nickname, timestamp);
+    const time = moment(timestamp).format('D-M-yyyy hh:mm:ss');
+    const messageToSend = `chatMessage: ${time} ${nickname} Say: ${chatMessage}`;
+    socket.emit('message', messageToSend);
+    socket.broadcast.emit('message', messageToSend);
+  });
+  // change name
+  socket.on('changeName', (data) => {
+    const id = data.socketID;
+    const userName = userInObj;
+    userName[id] = data.newNickname;
+    const newNick = Object.values(userName);
+    socket.broadcast.emit('changeName', { newNickname: newNick });
+    socket.emit('changeName', { newNickname: newNick });
   });
 
-  socket.on('message', ({ chatMessage, nickname }) => {
-    const timesTamp = new Date();
-    const time = moment(timesTamp).format('D-M-yyyy hh:mm:ss');
-    const renderMessagens = `${nickname} ${time} ${chatMessage}`;
-
-    socket.emit('message', renderMessagens);
-    socket.broadcast.emit('message', renderMessagens);
-
-    return saveMessage(chatMessage, nickname, time);
+  socket.on('disconnect', () => {
+    delete userInObj[socket.id];
+    socket.emit('changeName', { newNickname: Object.values(userInObj) });
   });
 });
 
-server.listen(PORT, () => {
-  console.log(`Ouvindo na porta ${PORT}`);
-});
+http.listen(3000, () => console.log('Servidor ouvindo na porta 3000'));
+// Alteração do todo o código com ajuda do Wilian por motivo de não conseguir passar no requisito 4.
