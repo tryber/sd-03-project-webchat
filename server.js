83cdf5604f336a3cb3bbbad5f3d90dd66f3f6a6f
Gustavo <ggteodor07@gmai.com>
2020-11-09 10:36:15 -0300

    time to correct the errors

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const io = require('socket.io')(http);
 const path = require('path')
 const { getAllmessages, saveMessage } = require('./models/messageModels')
 
+let arrUsers = [];
+
 app.get('/', (_req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
@@ -13,6 +15,8 @@ app.get('/', (_req, res) => {
 io.on('connection', async (socket) => {
   const savedMessages = await getAllmessages();
   socket.emit('history', savedMessages);
+  const id = arrUsers.length + 1;
+
 
   socket.on('message', async ({ chatMessage, nickname }) => {
     const date = new Date();
@@ -20,6 +24,29 @@ io.on('connection', async (socket) => {
     const msgTime = date.toLocaleTimeString('pt-BR');
     await saveMessage(msgTime, msgDate, chatMessage, nickname);
     io.emit('message', `${msgDate} ${msgTime} - ${nickname}: ${chatMessage}`);
+  });
+
+  socket.on('newUser', (nickname) => {
+    arrUsers.push({id, nickname});
+    io.emit('onlineList', arrUsers);
+    console.log(arrUsers);
+  });
+
+  socket.on('changeNick', async (newNick) => {
+    // arrUsers = await arrUsers.reduce((acc, elem) => {
+    //   if (elem.id !== id) {
+    //     return acc.push(elem);
+    //   }
+    //   return acc.push({ id: elem.id, newNick });
+    // }, []);
+    const index = arrUsers.findIndex((elem) => elem.id === id);
+    if (arrUsers[index]) arrUsers[index].nickname = newNick;
+    io.emit('onlineList', arrUsers);
+  });
+
+  socket.on('disconnect', () => {
+    arrUsers = arrUsers.filter((elem) => elem.id !== id);
+    io.emit('onlineList', arrUsers);
   });
 
 });
