729bbfc4fe75506b4b52800b23501a4ffff4edbb
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-11-09 14:07:35 -0300

    sugestão diego

--- model/connection.js
+++ model/connection.js
@@ -1,15 +1,17 @@
-const mongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
 let schema = null;
 
 async function connection() {
   if (schema) return Promise.resolve(schema);
-  return mongoClient
-    .connect(process.env.DB_URL, {
+  return MongoClient.connect(
+    process.env.DB_URL || 'mongodb://localhost:27017/webchat',
+    {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    })
+    }
+  )
     .then((conn) => conn.db(process.env.DB_NAME))
     .then((dbSchema) => {
       schema = dbSchema;
