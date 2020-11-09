bf1301d9ff3e891ee95ed18489f2e2d329c9ef93
DiegoRafaelAF <draf0314@gmail.com>
2020-11-09 13:54:41 -0300

    Alteraçao do banco requisito 4

--- models/connection.js
+++ models/connection.js
@@ -7,7 +7,7 @@ async function connection() {
   if (schema) return Promise.resolve(schema);
 
   return MongoClient
-    .connect(process.env.DB_URL || 'mongodb://localhost:27017', {
+    .connect(process.env.DB_URL || 'mongodb://localhost:27017/webchat', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
