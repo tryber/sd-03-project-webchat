163d50653e1e276894253f57d09ff93cd8f4324e
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-11-09 14:26:30 -0300

    alteração banco

--- model/connection.js
+++ model/connection.js
@@ -1,17 +1,15 @@
-const { MongoClient } = require('mongodb');
+const mongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
 let schema = null;
 
 async function connection() {
   if (schema) return Promise.resolve(schema);
-  return MongoClient.connect(
-    process.env.DB_URL || 'mongodb://localhost:27017/webchat',
-    {
+  return mongoClient
+    .connect(process.env.DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    },
-  )
+    })
     .then((conn) => conn.db(process.env.DB_NAME))
     .then((dbSchema) => {
       schema = dbSchema;
