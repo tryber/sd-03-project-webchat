5b0441c19f8a023f09616b95e9a414647ed6e1a0
luldelrey <rpgstrikered@gmail.com>
2020-11-16 01:59:40 -0300

    now i kinda know what im doing, here we goooooo

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log(true);
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (_req, res) => res.sendFile(`${__dirname}/index.html`));
+
+io.on('connection', (socket) => {
+  // aki fazer o user ficar on
+  // aki fazer o user ficar off
+  socket.on('disconnect', () => {});
+  // aki ele emite pra geral a mensagem
+  socket.on('message', ({ chatMessage, nickname }) => {
+    //salvar mensagem no banco de dados e retornar a hora
+    const time = '21/10/1999 05:23'
+    io.emit('serverMessage', `${nickname}${time}${chatMessage}`);
+  });
+});
+
+http.listen(3000, () => console.log('Listening on 3000!'));
