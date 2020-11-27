eb30fa52b67f7a08871c9140d88535d10603739f
mirellaalves <mirella.alves@gmail.com>
2020-11-27 15:32:03 -0300

    install dependences, config dotenv and start express

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+// const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+// const io = socketIo(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Lintening on ${PORT}`);
+});
