0d89b8336826d9c45260ad937ec195a1919f3bd1
Henrique Rezende <henriquerezendec@gmail.com>
2020-11-30 22:19:24 -0300

    Fix requisito 1

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,15 +1,15 @@
 const messageModel = require('../models/messageModel');
 
-const newMessage = (io) => async ({ nickname, chatMessage, room }) => {
+const newMessage = (io) => async ({ nickname, chatMessage, room = 'public' }) => {
   const sentOn = new Date();
   const sentOnDate = sentOn.toLocaleDateString('pt-BR').replace(/\//g, '-');
   const sentOnTime = sentOn.toLocaleTimeString('pt-BR');
-  const message = `${nickname},${chatMessage},${sentOnDate},${sentOnTime},${room}`;
+  const message = `${nickname}, ${chatMessage}, ${sentOnDate}, ${sentOnTime}, ${room}`;
   await messageModel.saveMessage(message, room);
-  if (room === 'public') {
-    io.emit('message', message);
-  } else {
+  if (room !== 'public') {
     io.to(room).emit('message', message);
+  } else {
+    io.emit('message', message);
   }
 };
 
