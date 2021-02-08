d44e278a34a989e2939a3aa48fb607d3f6de1444
rdgbrezolin <brezolinr@gmail.com>
2021-02-08 13:51:44 -0300

    fix -localOk

---
+++ controllers/messageController.js
@@ -0,0 +1,59 @@
+const services = require('../services/messagesService');
+
+// Fonte formato de data: https://stackoverflow.com/questions/10211145/getting-current-date-and-time-in-javascript
+const emitNewMessage = (io) => async ({ nickname, chatMessage }) => {
+  const currentDate = new Date();
+  const formattedDate = `
+    ${currentDate.getDate()}-${currentDate.getMonth() + 1}-${currentDate.getFullYear()}
+    ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}
+  `;
+  const message = `${nickname}: ${chatMessage} ${formattedDate}`;
+  io.emit('message', message);
+  const chatRoom = await services.getChatRoomByNumber(1);
+  if (!chatRoom) {
+    await services.newChatRoomAndSaveMessage({ nickname, chatMessage: message }, 1);
+    return;
+  }
+
+  await services.saveMessage({ nickname, chatMessage: message }, 1);
+};
+
+const getAllMessages = async () => {
+  const chatRoom = await services.getChatRoomByNumber(1);
+
+  if (chatRoom === null) return [];
+
+  return chatRoom.messagesArray;
+};
+
+const getPrivateMessages = async (id1, id2) => {
+  const returnPrivateMessages = await services.getPrivateMessages(id1, id2);
+
+  if (returnPrivateMessages === null) return [];
+
+  return returnPrivateMessages.messagesArray;
+};
+
+const saveMsgPrivate = async (id1, id2, { nickname, chatMessage }) => {
+  const currentDate = new Date();
+  const formattedDate = `
+    ${currentDate.getDate()}-${currentDate.getMonth() + 1}-${currentDate.getFullYear()}
+    ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}
+  `;
+  const message = `${nickname}: ${chatMessage} ${formattedDate}`;
+  const privateChatRoom = await services.getPrivateMessages(id1, id2);
+  if (!privateChatRoom) {
+    await services.newPrivateChatRoomAndSaveMessage(
+      id1, id2, { nickname, chatMessage: message },
+    );
+    return;
+  }
+  await services.savePrivateMessage(id1, id2, { nickname, chatMessage: message });
+};
+
+module.exports = {
+  emitNewMessage,
+  getAllMessages,
+  getPrivateMessages,
+  saveMsgPrivate,
+};
---
+++ services/messagesService.js
@@ -0,0 +1,43 @@
+const models = require('../models/messagesModel');
+
+const newChatRoomAndSaveMessage = async (msgObj, room) => {
+  const createdChatRoom = await models.newChatRoomAndSaveMessage(msgObj, room);
+  return createdChatRoom;
+};
+
+const newPrivateChatRoomAndSaveMessage = async (id1, id2, msgObj) => {
+  const savedMessage = await models.newPrivateChatRoomAndSaveMessage(
+    id1, id2, msgObj,
+  );
+  return savedMessage;
+};
+
+const getChatRoomByNumber = async (room) => {
+  const chatRoom = await models.getChatRoomByNumber(room);
+
+  return chatRoom;
+};
+
+const saveMessage = async (msgObj, room) => {
+  const newMessage = await models.saveMessageOnDb(msgObj, room);
+  return newMessage;
+};
+
+const savePrivateMessage = async (id1, id2, msgObj) => {
+  const savedMessage = await models.savePrivateMessage(id1, id2, msgObj);
+  return savedMessage;
+};
+
+const getPrivateMessages = async (id1, id2) => {
+  const privateMessages = await models.getPrivateMessages(id1, id2);
+  return privateMessages;
+};
+
+module.exports = {
+  saveMessage,
+  getChatRoomByNumber,
+  newChatRoomAndSaveMessage,
+  getPrivateMessages,
+  newPrivateChatRoomAndSaveMessage,
+  savePrivateMessage,
+};
