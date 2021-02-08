c4fcb5ad633337f4702f7171f11bf5ade1ef3e81
rdgbrezolin <brezolinr@gmail.com>
2021-02-08 11:35:39 -0300

    style

--- services/messagesService.js
+++ services/messagesService.js
@@ -1,36 +1,36 @@
-const messagesModel = require('../models/messagesModel');
+const models = require('../models/messagesModel');
 
 const saveMessage = async (messageObj, room) => {
-  const newMessage = await messagesModel.saveMessageOnDb(messageObj, room);
+  const newMessage = await models.saveMessageOnDb(messageObj, room);
   return newMessage;
 };
 
+const savePrivateMessage = async (id1, id2, messageObj) => {
+  const savedMessage = await models.savePrivateMessage(id1, id2, messageObj);
+  return savedMessage;
+};
+
 const getChatRoomByNumber = async (room) => {
-  const chatRoom = await messagesModel.getChatRoomByNumber(room);
+  const chatRoom = await models.getChatRoomByNumber(room);
 
   return chatRoom;
 };
 
 const createChatRoomAndSaveMessage = async (messageObj, room) => {
-  const createdChatRoom = await messagesModel.createChatRoomAndSaveMessage(messageObj, room);
+  const createdChatRoom = await models.createChatRoomAndSaveMessage(messageObj, room);
   return createdChatRoom;
 };
 
-const getPrivateMessages = async (id1, id2) => {
-  const privateMessages = await messagesModel.getPrivateMessages(id1, id2);
-  return privateMessages;
-};
-
 const createPrivateChatRoomAndSaveMessage = async (id1, id2, messageObj) => {
-  const savedMessage = await messagesModel.createPrivateChatRoomAndSaveMessage(
+  const savedMessage = await models.createPrivateChatRoomAndSaveMessage(
     id1, id2, messageObj,
   );
   return savedMessage;
 };
 
-const savePrivateMessage = async (id1, id2, messageObj) => {
-  const savedMessage = await messagesModel.savePrivateMessage(id1, id2, messageObj);
-  return savedMessage;
+const getPrivateMessages = async (id1, id2) => {
+  const privateMessages = await models.getPrivateMessages(id1, id2);
+  return privateMessages;
 };
 
 module.exports = {
